4a6b57e79f19ac6b588d97292e7d93b28287bf5b
oms125 <silsbeeowen@gmail.com>
2025-12-22 12:42:50 -0500

    init

---
+++ src/client/java/com/qolcraft/QOLCraftClient.java
@@ -0,0 +1,10 @@
+package com.qolcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class QOLCraftClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/qolcraft/QOLCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package com.qolcraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class QOLCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
